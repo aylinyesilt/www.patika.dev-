7b01721afec84417d6d018004c4e598770fd27f7
aylinyesilt <108435979+aylinyesilt@users.noreply.github.com>
2022-06-30 01:10:12 +0300

    Create odev1.sql

---
+++ odev1.sql
@@ -0,0 +1,5 @@
+select title, description from film ;
+select * from film where length > 60 or lenght < 75 ;
+select * from dilm where rental_rate = 0.99 and replacement cost = 12.99 or replacement cost = 28.99 ;
+select first_name, last_name from customer where first name = 'Mary' ;
+select * from film where not length > 50 and not(rental_rate = 2.99 or rental_rate = 4.99)
